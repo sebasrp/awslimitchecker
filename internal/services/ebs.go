37d6449ed3817f936110c3cdeff01a485e75250f
Sebastian Rodriguez <331558+sebasrp@users.noreply.github.com>
2022-10-02 23:28:51 +0800

    add ebs quotas for io1 iops and size

--- internal/services/ebs.go
+++ internal/services/ebs.go
@@ -10,9 +10,11 @@ import (
 func NewEbsChecker() Svcquota {
 	serviceCode := "ebs"
 	supportedQuotas := map[string]func(ServiceChecker) (ret []AWSQuotaInfo){
-		"Snapshots per Region": ServiceChecker.getEbsSnapshotsUsage,
+		"Snapshots per Region":                                   ServiceChecker.getEbsSnapshotsUsage,
+		"IOPS for Provisioned IOPS SSD (io1) volumes":            ServiceChecker.getEbsIo1IopsUsage,
+		"Storage for Provisioned IOPS SSD (io1) volumes, in TiB": ServiceChecker.getEbsIo1SizeUsage,
 	}
-	requiredPermissions := []string{"ec2:DescribeSnapshots"}
+	requiredPermissions := []string{"ec2:DescribeSnapshots", "ec2:DescribeVolumes"}
 
 	return NewServiceChecker(serviceCode, supportedQuotas, requiredPermissions)
 }
@@ -34,4 +36,65 @@ func (c ServiceChecker) getEbsSnapshotsUsage() (ret []AWSQuotaInfo) {
 
 	ret = append(ret, quotaInfo)
 	return
+}
+
+func (c ServiceChecker) getEbsIo1IopsUsage() (ret []AWSQuotaInfo) {
+	ret = []AWSQuotaInfo{}
+
+	iops, _, err := getEbsVolumeDetails("io1")
+	if err != nil {
+		fmt.Printf("failed to retrieve ec2 ebs io1 volumes, %v", err)
+		return
+	}
+	quotaInfo := c.GetAllAppliedQuotas()["IOPS for Provisioned IOPS SSD (io1) volumes"]
+	quotaInfo.UsageValue = float64(iops)
+
+	ret = append(ret, quotaInfo)
+	return
+}
+
+func (c ServiceChecker) getEbsIo1SizeUsage() (ret []AWSQuotaInfo) {
+	ret = []AWSQuotaInfo{}
+
+	_, size, err := getEbsVolumeDetails("io1")
+	if err != nil {
+		fmt.Printf("failed to retrieve ec2 ebs io1 volumes, %v", err)
+		return
+	}
+	quotaInfo := c.GetAllAppliedQuotas()["Storage for Provisioned IOPS SSD (io1) volumes, in TiB"]
+	quotaInfo.UsageValue = GiBtoTiB(float64(size))
+
+	ret = append(ret, quotaInfo)
+	return
+}
+
+func getEbsVolumeDetails(volumeType string) (iops int, size int, err error) {
+	iops = 0
+	size = 0
+
+	volumes := []*ec2.Volume{}
+	err = conf.Ec2.DescribeVolumesPages(
+		&ec2.DescribeVolumesInput{
+			Filters: []*ec2.Filter{
+				{Name: aws.String("volume-type"),
+					Values: []*string{aws.String(volumeType)},
+				}}},
+		func(p *ec2.DescribeVolumesOutput, lastPage bool) bool {
+			volumes = append(volumes, p.Volumes...)
+			return true // continue paging
+		})
+	if err != nil {
+		fmt.Printf("failed to retrieve ec2 ebs volumes, %v", err)
+		return
+	}
+
+	for _, v := range volumes {
+		iops += int(*v.Iops)
+		size += int(*v.Size)
+	}
+	return
+}
+
+func GiBtoTiB(gib float64) (ret float64) {
+	return gib / 1024
 }
